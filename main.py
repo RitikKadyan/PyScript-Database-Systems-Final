eca523ac4a00231f68aaa3b27894b37780e68b32
Ritik Kadyan <74025554+RitikKadyan@users.noreply.github.com>
2022-12-15 14:43:47 -0500

    'update'

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import sqlite3
 import pandas as pd
-from js import console
+#from js import console
 #python -m http.server 80 | Command to host local server | localhost/file name
 
 
 def showGames(*ags, **kags):
-    db = sqlite3.connect("data.db")
+    db = sqlite3.connect("dat.db")
     c = db.cursor()
 
     sql_query = pd.read_sql_query('''SELECT * FROM games''', db)
     df = pd.DataFrame(sql_query, columns=['g_id', 'g_name', "g_qty_sold"])
     df.rename(columns={'g_id': 'ID', 'g_name': 'Name', 'g_qty_sold': 'Qty Sold'}, inplace=True)
-    print(df.to_html(index=False))
-    console.log(df)
+    return df.to_html(index=False)
+    # print(df.to_html(index=False))
+    #console.log(sql_query)
     c.close()
     db.close()
     
 def test_show_games():
-    db = sqlite3.connect("data.db")
+    db = sqlite3.connect("dat.db")
 
     sql_query = pd.read_sql_query('''SELECT * FROM games''', db)
     df = pd.DataFrame(sql_query, columns=['g_id', 'g_name', "g_qty_sold"])
@@ -27,7 +28,7 @@ def test_show_games():
     db.close()
 
 def insertIntoGames(*args, **kwargs):
-    db = sqlite3.connect("data.db")
+    db = sqlite3.connect("dat.db")
     c = db.cursor()
 
     game_input = str(Element('game-input').element.value)
@@ -39,8 +40,9 @@ def insertIntoGames(*args, **kwargs):
     #db.execute(sql, (game_input.element.value, game_qty_input.element.value))
     #c.execute('''INSERT INTO games VALUES (null, ?, ?), (game_input.value, game_qty_input.value)''')
     c.execute("INSERT INTO games (g_name, g_qty_sold) VALUES ('{}', {})".format(game_input, game_qty_input))
-    print(f"executed {game_input}, {game_qty_input}")
     db.commit()
+    print(f"executed {game_input}, {game_qty_input}")
+    
     #print("commited")
     #print(game_input.element.value)
 
@@ -49,7 +51,7 @@ def insertIntoGames(*args, **kwargs):
 
     
 def test_insert():
-    db = sqlite3.connect("data.db")
+    db = sqlite3.connect("dat.db")
     c = db.cursor()
 
     game = "\"Black Ops\""
@@ -62,12 +64,15 @@ def test_insert():
 
 
 def deleteFromGames(*ags, **kags):
-    db = sqlite3.connect("data.db")
+    db = sqlite3.connect("dat.db")
     c = db.cursor()
 
-    c.execute("DELETE FROM games WHERE g_id = ?", (4,))
+    c.execute("DELETE FROM games WHERE g_id = ?", (3,))
     db.commit()
 
     c.close()
     db.close()
 
+
+#test_insert()
+test_show_games()
